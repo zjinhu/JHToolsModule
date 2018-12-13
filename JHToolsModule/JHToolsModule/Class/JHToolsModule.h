624af860827a162469f91bd9af835c04eaf39f65
jinhu zhang <814030966@qq.com>
2018-12-13 14:43:33 +0800

    release

--- JHToolsModule/JHToolsModule/Class/JHToolsModule.h
+++ JHToolsModule/JHToolsModule/Class/JHToolsModule.h
@@ -10,15 +10,36 @@
 #define JHToolsModule_h
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+
+#if __has_include(<JHToolsModule/JHToolsModule.h>)
+
+#import <JHToolsModule/EdgeInsetsLabel.h>
+#import <JHToolsModule/JHBaseViewController.h>
+#import <JHToolsModule/JHBaseTableViewController.h>
+#import <JHToolsModule/JHBaseWebViewController.h>
+#import <JHToolsModule/JHToolsDefine.h>
+#import <JHToolsModule/BaseUI.h>
+#import <JHToolsModule/SafeEX.h>
+#import <JHToolsModule/MasonryUI.h>
+#import <JHToolsModule/UIButton+Position.h>
+#import <JHToolsModule/NSObject+BlockSEL.h>
+#import <JHToolsModule/UIControl+Block.h>
+#import <JHToolsModule/NLog.h>
+#import <JHToolsModule/EasyShow.h>
+#import <JHToolsModule/GPSLocationManager.h>
+#import <JHToolsModule/UILineView.h>
+#import <JHToolsModule/UserDefaults.h>
+#import <JHToolsModule/NSArray+Sudoku.h>
+
+#else
 #import "EdgeInsetsLabel.h"
 #import "JHBaseViewController.h"
 #import "JHBaseTableViewController.h"
 #import "JHBaseWebViewController.h"
 #import "JHToolsDefine.h"
 #import "BaseUI.h"
-
 #import "SafeEX.h"
-#import "MasonryUI.h" 
+#import "MasonryUI.h"
 #import "UIButton+Position.h"
 #import "NSObject+BlockSEL.h"
 #import "UIControl+Block.h"
@@ -28,4 +49,7 @@
 #import "UILineView.h"
 #import "UserDefaults.h"
 #import "NSArray+Sudoku.h"
+#endif
+
+
 #endif /* JHToolsModule_h */
