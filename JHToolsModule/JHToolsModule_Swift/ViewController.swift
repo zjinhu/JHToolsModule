66659cd58fe5b9170aa73ff52e727f94c3e40219
jinhu.zhang <jinhu.zhang@ikang.com>
2019-11-18 18:38:27 +0800

    no message

--- JHToolsModule/JHToolsModule_Swift/ViewController.swift
+++ JHToolsModule/JHToolsModule_Swift/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -17,8 +17,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         SLog(StatusBarHeight())
 
+        let imageView = UIImageView.init(image: JHImageLoader.loadToolsImage(with: "nav_ic_back"))
+        self.view.addSubview(imageView)
+        imageView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(50)
+            make.height.width.equalTo(100)
+            make.centerX.equalToSuperview()
+        }
+        
+        let btn = UIButton.init()
+        btn.setTitle("123", for: .normal)
+        self.view.addSubview(btn)
+        btn.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(150)
+            make.height.width.equalTo(100)
+            make.centerX.equalToSuperview()
+        }
+        btn.addTarget(self, action: #selector(jupm), for: .touchUpInside)
+        
     }
 
-
+    @objc func jupm(){
+        let vc = TestViewController.init()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
